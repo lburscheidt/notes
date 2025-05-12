7b4791267fdf0e3e27f0b6b1b532f9707f157f86
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2025-05-12 21:03:57 +0200

    changed port variable

--- backend/index.js
+++ backend/index.js
@@ -90,6 +90,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`)
-  })
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
